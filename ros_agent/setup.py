157cbffe6e6b0a3078d6e983884670bcf029b5e2
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2024-04-04 16:24:06 +0200

    Add resource to resolve colcon warning

--- ros_agent/setup.py
+++ ros_agent/setup.py
@@ -11,8 +11,8 @@ setup(
  version='0.0.0',
  packages=["agents", "models", "helpers"],
  data_files=[
-     #('share/ament_index/resource_index/packages',
-     #        ['resource/' + package_name]),
+     ('share/ament_index/resource_index/packages',
+             ['resource/' + package_name]),
      ('share/' + package_name, ['package.xml']),
    ],
  install_requires=['setuptools'],
